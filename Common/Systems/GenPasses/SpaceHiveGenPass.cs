5e0fdd5d9bea7ca4c15dea5c838b594c5ad58bf9
Wojtanello-coder <walonale@gmail.com>
2022-10-30 19:49:45 +0100

    WorldGen space rocks/worldedit tools: zx

---
+++ Common/Systems/GenPasses/SpaceHiveGenPass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace BeeStation.Common.Systems.GenPasses
+{
+    class SpaceHiveGenPass : GenPass
+    {
+	  public SpaceHiveGenPass(string name, float weight) : base(name, weight)
+	  {
+
+	  }
+
+	  protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+	  {
+		progress.Message = "Generating More Bee Hives";
+
+		int maxtoSpawn = (int)(Main.maxTilesX * 0.001);
+		for(int i = 0; i < maxtoSpawn; i++)
+		{
+		    int X = WorldGen.genRand.Next(200, Main.maxTilesX - 200);
+		    int Y = Math.Abs(WorldGen.genRand.Next(50, 50));//(int)Main.worldSurface * 0.35f)); //(int)WorldGen.worldSurface
+
+		    int radius = 15;
+		    for (int x = X - radius; x <= X + radius; x++)
+			  for (int y = Y - radius; y <= Y + radius; y++)
+				if (Vector2.Distance(new Vector2(X, Y), new Vector2(x, y)) <= radius)
+				    WorldGen.PlaceTile(X, Y, TileID.Dirt, false, true);
+		    
+		    //WorldGen.TileRunner(x, y, 10, 3, TileID.TopazGemspark, true);
+		}
+		//int X = Main.spawnTileX;
+		//int Y = Main.spawnTileY - 50;
+
+	  }
+    }
+}
---
+++ Common/Systems/GenPasses/WorldGenTutorialWorld.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Input;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.WorldBuilding;
+//using Terraria.World.Generation;
+using Terraria.GameContent.Generation;
+
+namespace WorldGenTutorial
+{
+    class WorldGenTutorialWorld : ModPlayer
+    {
+	  public static bool JustPressed(Keys key)
+	  {
+		return Main.keyState.IsKeyDown(key) && !Main.oldKeyState.IsKeyDown(key);
+	  }
+
+	  public override void PostUpdate()
+	  {
+		if (JustPressed(Keys.Z))
+		    TestMethod((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
+		if (JustPressed(Keys.X))
+		    TestMethod2((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
+	  }
+
+	  private void TestMethod(int x, int y)
+	  {
+		Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
+
+		// Code to test placed here:
+		WorldGen.digTunnel(x, y, 1, 0, 30, 2);
+	  }
+	  private void TestMethod2(int x, int y)
+	  {
+		Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
+
+		// Code to test placed here:
+		//WorldGen.TileRunner(x + xx, y + yy, 5, 5, TileID.Dirt, true);
+
+		Point point = new Point(x, y);
+		WorldUtils.Gen(point, new Shapes.Tail(5, new Vector2(WorldGen.genRand.Next(-10, 10), WorldGen.genRand.Next(15, 30))), new Actions.SetTile(TileID.Dirt));
+	  }
+    }
+}
