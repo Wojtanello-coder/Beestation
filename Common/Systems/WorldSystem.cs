f6d5732b9fa44745b069964700ab797e2277a840
Wojtanello-coder <walonale@gmail.com>
2022-10-30 19:49:45 +0100

    WorldGen space rocks/worldedit tools: zx

---
+++ Common/Systems/WorldSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+using System.Collections.Generic;
+using BeeStation.Common.Systems.GenPasses;
+
+namespace BeeStation.Common.Systems
+{
+    class WorldSystem : ModSystem
+    {
+	  public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+	  {
+		int ShiniesIndex = tasks.FindIndex(t => t.Name.Equals("Shinies"));
+		if(ShiniesIndex != -1)
+		{
+		    tasks.Insert(ShiniesIndex + 1, new SpaceHiveGenPass("Space Hive", 320f));
+		}
+	  }
+    }
+}
