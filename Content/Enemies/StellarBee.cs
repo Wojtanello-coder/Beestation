c136e926ac0cecfcbd926c569da21c9aeeedf2a3
Wojtanello-coder <walonale@gmail.com>
2025-11-23 21:20:05 +0100

    StellarBeeGuardian AI Concept

---
+++ Content/Enemies/StellarBee.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeeStation.Content.Enemies
+{
+    internal class StellarBee : ModNPC
+    {
+        public NPC FollowingNPC => Main.npc[(int)NPC.ai[0]];
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 2;
+        }
+        public override void SetDefaults()
+        {
+
+            NPC.width = 32;
+            NPC.height = 32;
+            NPC.damage = 30;
+            NPC.defense = 10;
+            NPC.lifeMax = 100;
+            NPC.value = 0f;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+        }
+        public override void AI()
+        {
+            float maxSpeed = 10f;
+            float float_distance = 400f;
+            if (FollowingNPC.life <= 0)
+            {
+            NPC.StrikeInstantKill();
+            }
+            else
+            {
+                Microsoft.Xna.Framework.Vector2 following = FollowingNPC.Center;
+
+                NPC.rotation += 1f / 60;
+                following.X += (float)Math.Sin(NPC.rotation) * float_distance;
+                following.Y -= (float)Math.Cos(NPC.rotation) * float_distance;
+                if (NPC.Center.X > following.X) NPC.velocity.X += (following.X - NPC.Center.X) / 10;
+                if (NPC.Center.X < following.X) NPC.velocity.X += (following.X - NPC.Center.X) / 10;
+                if (NPC.Center.Y > following.Y) NPC.velocity.Y += (following.Y - NPC.Center.Y) / 10;
+                if (NPC.Center.Y < following.Y) NPC.velocity.Y += (following.Y - NPC.Center.Y) / 10;
+                if (Math.Abs(NPC.velocity.Length()) > maxSpeed) {
+                    NPC.velocity.Normalize();
+                    NPC.velocity *= maxSpeed;
+                }
+            }
+        }
+    }
+}
---
+++ Content/Enemies/StellarBeeGuardian.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeeStation.Content.Enemies
+{
+    internal class StellarBeeGuardian : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 3;
+        }
+        public override void SetDefaults()
+        {
+
+            NPC.width = 32;
+            NPC.height = 48;
+            NPC.damage = 20;
+            NPC.defense = 10;
+            NPC.lifeMax = 20;
+            NPC.value = 2850f;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return 1f;
+
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            NPC.ai[0] = 0; // Summon Cooldown
+            NPC.ai[1] = 0; // Summon Count
+            NPC.ai[2] = 0; 
+            NPC.ai[3] = 0;
+        }
+        public override void AI()
+        {
+            if (Main.player[NPC.FindClosestPlayer()].Distance(NPC.Center) < 320f && NPC.ai[0] <= 0 && NPC.ai[1] < 7) {
+                NPC.ai[0] = 360 * (float)Math.PI / 7;
+                NPC.ai[1]++;
+                NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.Center.X, (int)NPC.Center.Y, ModContent.NPCType<StellarBee>(), 0, NPC.whoAmI);
+            }
+            if (NPC.ai[0] > 0) NPC.ai[0]--;
+        }
+
+    }
+}
