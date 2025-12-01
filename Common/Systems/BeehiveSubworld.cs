5f44f138239800b0ea893bbaf48dfff837fb574e
Wojtanello-coder <walonale@gmail.com>
2025-12-01 03:22:01 +0100

    BeehiveSubworld Initialization

---
+++ Common/Systems/BeehiveSubworld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeeStation.Common.Systems.GenPasses.BeehiveSubworld;
+using SubworldLibrary;
+using Terraria.WorldBuilding;
+
+namespace BeeStation.Common.Systems
+{
+    class BeehiveSubworld : Subworld
+    {
+        public override int Width => 840;
+        public override int Height => 840;
+        public override bool ShouldSave => true;
+        public override bool NoPlayerSaving => true;
+        public override List<GenPass> Tasks => new List<GenPass>()
+        {
+            new BeehiveGroundGenPass("Ground", 1f)
+        };
+
+        
+    }
+}
---
+++ Common/Systems/GenPasses/BeehiveSubworld/BeehiveGroundGenPass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+
+namespace BeeStation.Common.Systems.GenPasses.BeehiveSubworld
+{
+    internal class BeehiveGroundGenPass : GenPass
+    {
+        public BeehiveGroundGenPass(string name, float weight) : base(name, weight) { }
+
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
